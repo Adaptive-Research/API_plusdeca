951a6d6bcb8d59d10709946036e864becc79ef13
Adaptive-Research <ddupard@adaptive-research.eu>
2023-01-06 01:27:29 +0100

    0.02

--- SQL/postgreSQL/Network/Event.sql
+++ SQL/postgreSQL/Network/Event.sql
@@ -1,60 +1,65 @@
+
 drop table if exists events_type ;
 create table events_type (
    id BIGSERIAL PRIMARY KEY,
-   event_type int not null,
-   sorder int null,
-   valuelangue varchar(3),
+   event_type smallint not null,
+   sorder smallint null,
+   valuelangue varchar(3) DEFAULT 'FR',
    nom varchar(200)
 ) ;
 
-insert into events_type (event_type, sorder,valuelangue,nom) values (1,1,'FR','réunions d''entrepreneurs') ;
+insert into events_type (event_type, sorder,valuelangue,nom) values (1,1,'FR','réunion de groupe') ;
 insert into events_type (event_type, sorder,valuelangue,nom) values(2,2,'FR','salon') ;
-insert into events_type (event_type, sorder,valuelangue,nom) values(3,3,'FR','rendez-vous client') ;
-insert into events_type (event_type, sorder,valuelangue,nom) values(4,4,'FR','webinaire') ;
+insert into events_type (event_type, sorder,valuelangue,nom) values(3,3,'FR','webinaire') ;
 insert into events_type (event_type, sorder,valuelangue,nom) values(999,999,'FR','autre') ;
-
 
 
 
 create table events_format (
    id BIGSERIAL PRIMARY KEY,
-   event_type int not null,
-   event_format int not null,
+   event_type smallint not null,
+   event_format smallint not null,
    
-   sorder int null,
-   valuelangue varchar(3),
+   sorder smallint ,
+   valuelangue varchar(3) DEFAULT 'FR',
    nom varchar(200)
 ) ;
 
-insert into events_format (event_type,event_format,sorder,valuelangue,nom) values (1,1,1,'FR','présentiel format libre') ;
-insert into events_format (event_type,event_format,sorder,valuelangue,nom) values(1,2,2,'FR','présentiel en petits groupes') ;
-insert into events_type (event_type,event_format,sorder,valuelangue,nom) values(1,3,3,'FR','présentiel speed dating') ;
-insert into events_type (event_type,event_format,sorder,valuelangue,nom) values(1,4,4,'FR','en ligne') ;
-insert into events_type (event_type,event_format,sorder,valuelangue,nom) values(1,5,5,'FR','mixte (présentiel et en ligne)') ;
+-- ce sont les types de format en fonction du type d'evenement
+insert into events_format (event_type,event_format,sorder,valuelangue,nom) values (1,1,1,'FR','présentiel') ;
+insert into events_type (event_type,event_format,sorder,valuelangue,nom) values(1,2,2,'FR','en ligne') ;
+insert into events_type (event_type,event_format,sorder,valuelangue,nom) values(1,3,3,'FR','mixte (présentiel et en ligne)') ;
 
-insert into events_type (event_type,event_format,sorder,valuelangue,nom) values(4,4,4,'FR','en ligne') ;
+insert into events_format (event_type,event_format,sorder,valuelangue,nom) values (2,1,1,'FR','présentiel') ;
+insert into events_format (event_type,event_format,sorder,valuelangue,nom) values (3,1,1,'FR','présentiel') ;
+insert into events_format (event_type,event_format,sorder,valuelangue,nom) values (3,4,2,'FR','téléphone') ;
+insert into events_type (event_type,event_format,sorder,valuelangue,nom) values(4,4,1,'FR','en ligne') ;
+
+insert into events_format (event_type,event_format,sorder,valuelangue,nom) values (999,1,1,'FR','présentiel') ;
+insert into events_type (event_type,event_format,sorder,valuelangue,nom) values(999,2,2,'FR','en ligne') ;
+insert into events_type (event_type,event_format,sorder,valuelangue,nom) values(999,3,3,'FR','mixte (présentiel et en ligne)') ;
 
 
 
 
--- ce sont tous les events qui existent
 
+-- ce sont tous les events qui existent. Cela comprend les salons que l'on recupere sur internet et les evenements orgaanises par nos membres
 
 drop table if exists events ;
 create table events (
   id BIGSERIAL PRIMARY KEY,
   iscurrent smallint default 1,
 
-  event_type int not null,
-  event_format int default null,
+  event_type smallint not null,
+  event_format smallint default null,
   event_title varchar(200)  not null,
   event_allday smallint default 0,
   event_start varchar(50),
   event_end varchar(50),
-  event_city varchar(200) not null,
-  event_location varchar(200) not null,
+  event_city varchar(200) not null,         -- c'est la ville pour geolocaliser
+  event_location varchar(500) not null,     -- c'est l'adresse un peu plus precise
 
-  event_data text default null,
+  event_data text default null,             -- cela sert a contenir des infos supplementaires sur l'evenement
   
   date_save timestamp default current_timestamp
 ) ;
@@ -62,9 +67,39 @@ create table events (
 
 
 
+drop table if exists events_type_organisateur ;
+create table events_type_organisateur (
+   id BIGSERIAL PRIMARY KEY,
+   type_organisateur smallint default 0,
+   sorder smallint null,
+   valuelangue varchar(3) DEFAULT 'FR',
+   nom varchar(200)
+) ;
+
+insert into events_type_organisateur (type_organisateur, sorder,valuelangue,nom) values (1,1,'FR','organisateur') ;
+insert into events_type_organisateur (type_organisateur, sorder,valuelangue,nom) values (2,2,'FR','co organisateur') ;
 
 
--- ce sont les evenements auxquels s'abonne un utilisateur
+
+
+
+drop table if exists events_organisateur ;
+create table events_organisateur (
+  id BIGSERIAL PRIMARY KEY,
+  iscurrent smallint default 1,
+
+  idevent bigint not null,
+  idutilisateur bigint not null,  -- l'utilisateur a organise ou co organise l'evenement
+
+  idtype_organisateur smallint ,  -- contient soit orgnaisateur soit co organisateur 
+  
+  date_save timestamp default current_timestamp
+) ;
+
+
+
+
+-- ce sont les evenements auxquels va un utilisateur
 
 drop table if exists myevents ;
 create table myevents (
@@ -72,9 +107,9 @@ create table myevents (
   iscurrent smallint default 1,
 
   idevent bigint not null,
-  idutilisateur bigint not null,  -- l'utilisateur s'est abonne a l'evenement
+  idutilisateur bigint not null,  -- l'utilisateur s'est abonne a l'evenement 
 
-  presence_start varchar(50), -- un evenbement peut durer 2 jours et l'utilisateur a perevu de n'y aller qu'une demi journee
+  presence_start varchar(50), -- un evenement peut durer 2 jours et l'utilisateur a prevu de n'y aller qu'une demi journee
   presence_end varchar(50),
 
   date_save timestamp default current_timestamp
@@ -83,16 +118,34 @@ create table myevents (
 
 
 
+drop table if exists activity_type ;
+create table activity_type (
+   id BIGSERIAL PRIMARY KEY,
+   activity_type smallint not null,
+   sorder smallint null,
+   valuelangue varchar(3) DEFAULT 'FR',
+   nom varchar(200)
+) ;
+
+insert into events_type (event_type, sorder,valuelangue,nom) values (1,1,'FR','réunion de groupe') ;
+insert into events_type (event_type, sorder,valuelangue,nom) values(2,2,'FR','salon') ;
+insert into events_type (event_type, sorder,valuelangue,nom) values(3,3,'FR','rendez-vous client') ;
+insert into events_type (event_type, sorder,valuelangue,nom) values(4,4,'FR','webinaire') ;
+insert into events_type (event_type, sorder,valuelangue,nom) values(999,999,'FR','autre') ;
+
+
 
 
 -- un utilisateur peut aller a des events, organiser des events, planifier une activite
-
+-- la table ci-dessous sert a planifier une activite 
 
 drop table if exists myactivities ;
 create table myactivities (
   id BIGSERIAL PRIMARY KEY,
   iscurrent smallint default 1,
 
+
+  idutilisateur bigint not null,  -- l'utilisateur s'est abonne a l'evenement
 
   activity_type int not null,
   activity_format int default null,
@@ -103,7 +156,7 @@ create table myactivities (
   activity_city varchar(200) not null,
   activity_location varchar(200) not null,
 
-  event_data text default null,
+  activity_data text default null,
   
   date_save timestamp default current_timestamp
 ) ;
---
+++ SQL/postgreSQL/Network/Groupe.sql
@@ -0,0 +1,33 @@
+
+
+drop table if exists groupe ;
+create table groupe (
+   id BIGSERIAL PRIMARY KEY,
+   idutilisateur bigint not null,  -- createur du groupe 
+   nom varchar(200),               -- nom du groupe
+   sdescription text               -- description de ce que fait le groupe
+   image bigint                    -- c'est l'image du groupe 
+) ;
+
+
+drop table if exists groupe_manager ;
+create table groupe_manager (
+   id BIGSERIAL PRIMARY KEY,
+   idgroupe bigint not null,                        -- id du groupe dont l'utilisateur est l'un des managers
+   idutilisateur bigint not null,                   -- manager du groupe
+   date_save timestamp default current_timestamp    -- date d'attribution du role de manager du groupe 
+ 
+) ;
+
+
+drop table if exists groupe_utilisateur ;
+create table groupe_utilisateur (
+   id BIGSERIAL PRIMARY KEY,
+   idgroupe bigint not null,                        -- id du groupe auquel est abonne l'utilisateur
+   idutilisateur bigint not null,                   -- membre du groupe
+   date_save timestamp default current_timestamp    -- date d'entree dans le groupe 
+ 
+) ;
+
+-- le premier groupe que je vaia creer c'est: Entreprendre en pays de Nemours
+
