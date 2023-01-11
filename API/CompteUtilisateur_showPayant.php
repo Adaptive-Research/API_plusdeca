7332274bca8d6d66c3663772959e8ea183338abf
Adaptive-Research <ddupard@adaptive-research.eu>
2023-01-11 16:36:37 +0100

    0.03

---
+++ API/CompteUtilisateur_showPayant.php
@@ -0,0 +1,115 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("content-type: application/json");
+
+
+class UtilisateurPayant_API
+{
+  public $id ;
+  public $idUtilisateur ;
+  public $idFonction ;
+  public $DateFin ;
+}
+
+
+
+include $baseAPI.'/RTY;456/config.php';
+include __DIR__.'/RTY;456/test.php';
+
+
+if (isset($_POST['debug'])) {
+  print_r($_POST) ;
+  echo "\n" ;
+}
+
+
+if ( isset($_POST['Submit']) ) 
+{
+
+  if (isset($_POST['token']))
+  {
+    if (isset($_POST['debug']))
+        echo "TokenSent: ".$_POST['token']."\n" ;
+    $pos = strpos($_POST['token'], ";");
+    if ($pos >= 0)
+    {
+      $idUser = trim(substr($_POST['token'],0,$pos)) ;
+      if (isset($_POST['debug']))
+        echo "idUser: ".$idUser."\n" ;
+
+      $token = trim(substr($_POST['token'],$pos+1)) ;
+      if (isset($_POST['debug']))
+        echo "token: ".$token."\n" ;
+
+
+      // Create connection
+      $conn_string = "host=".$servername." port=5432 dbname=".$dbname." user=".$username." password=".$password ;
+      $conn = pg_connect($conn_string);
+
+      $ip = getIPAddress();  
+      $sql = "insert into logpage (ip,page,method) values ('" ;
+      $sql = $sql.$ip."','".$_SERVER['REQUEST_URI']."','".$_SERVER['REQUEST_METHOD']."')" ;
+      $result = pg_query($conn, $sql);      
+
+
+      $sql = "select * from utilisateur_session where iduser = ".$idUser." and token = '".$token."' "  ;
+      if (isset($_POST['debug']))
+        echo $sql."\n" ;
+
+      $result = pg_query($conn, $sql);
+      $num_rows = pg_num_rows($result);
+      if ( $num_rows > 0 )        
+      {
+        $sql2 = "select * from utilisateur_payant where idutilisateur = ".$idUser ;
+
+        if (isset($_POST['debug']))
+         echo $sql2."\n" ;
+
+        $arr = [] ;
+
+        $result2 = pg_query($conn, $sql2);
+        $num_rows2 = pg_num_rows($result2);
+        if ( $num_rows2 > 0 )
+        {
+          while($row = pg_fetch_assoc($result2))
+          {
+            $objK = new UtilisateurPayant_API ;
+            $objK->id = $row['id'] ;
+            $objK->idUtilisateur = $row['idutilisateur'] ;
+            $objK->idFonction = $row['idfonction'] ;
+            $objK->DateFin = $row['date_fin'] ;
+
+           
+            array_push($arr,$objK) ;
+          }
+          echo json_encode($arr) ;
+        }
+        else 
+          echo "ERROR: No business card for this user" ;
+
+
+      }
+      else
+        echo "ERROR: Token unknown" ;
+    }
+    else
+      echo "ERROR: Token does not have the right format" ;
+
+  }
+  else
+    echo "ERROR: No token" ;
+
+}
+else
+  echo "ERROR: Submit not found" ;
+
+
+//$pg_close($conn);
+// il ne faut pas closer la connexion a la base car a la fin du script cela se fait automatiquement et 
+// si on le fait cela genere une message serveur de type 500
+
+
+?>
+
+
